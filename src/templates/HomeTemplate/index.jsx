e0c24fe5c2789dd68e43eb96b0c4d2368e7cbad5
Katsu837 <mp.200402@gmail.com>
2024-07-28 22:12:37 +0300

    Delete 's' from HomeTemplate

--- src/templates/HomeTemplate/index.jsx
+++ src/templates/HomeTemplate/index.jsx
@@ -6,7 +6,7 @@ const HomeTemplate = () => {
         <Grid2 container spacing={4}>
             <Grid2 xs={3}>
                 {/*<LeftSideBar/>*/}
-            </Grid2>s
+            </Grid2>
             <Grid2 container xs={9}>
                 <Grid2 xs={8}>
                     {/*<NavCalendar/>*/}
